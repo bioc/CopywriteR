e5cd19304a670affe837f37c950ba9ead19e3205
Thomas Kuilman <t.kuilman@nki.nl>
2015-04-20 10:20:38 +0000

    Bug fixes and update description
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/CopywriteR@102788 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/private.R
+++ R/private.R
@@ -2,6 +2,7 @@
     abs(x - round(x)) < tol
 }
 
+<<<<<<< HEAD
 .loadCovData <- function(files, gc = NULL, mapa = NULL, black = NULL,
                          excludechr = NULL, datacol = 5) {
     d <- lapply(files, read.delim, header = FALSE)
@@ -47,18 +48,32 @@
     list(cov = cov, anno = anno)
 }
 
+=======
+>>>>>>> master
 .peakCutoff <- function(cov, fdr.cutoff = 1e-04, k = 2:150) {
     length.y <- length(cov)
     y <- tabulate(cov)
     y <- append(y, length.y - sum(y), 0)
     names(y) <- 0:(length(y) - 1)
     z <- sort(y, decreasing = TRUE)
+<<<<<<< HEAD
     lambda <- as.integer(names(z[1])) + 1 # Add one as otherwise 0 is possible outcome
     second.largest <- as.integer(names(z[2])) + 1
     if (lambda == 1 & second.largest != 2) { # If second largest value after 0 is not 1, use other lambda
         lambda <- second.largest + 1
     }
     n <- exp(log(y[1] + 1) - dpois(1, lambda, log = TRUE)) # 1 Added to y[1]; otherwise if y[1] = 0 outcome is 0
+=======
+    # Add 1 as otherwise 0 is possible outcome
+    lambda <- as.integer(names(z[1])) + 1
+    second.largest <- as.integer(names(z[2])) + 1
+    # If second largest value after 0 is not 1, use other lambda
+    if (lambda == 1 & second.largest != 2) {
+        lambda <- second.largest + 1
+    }
+    # Add 1 to y[1]; otherwise if y[1] = 0 outcome is 0
+    n <- exp(log(y[1] + 1) - dpois(1, lambda, log = TRUE))
+>>>>>>> master
     exp.fd <- n * ppois(k - 1, lambda, lower.tail = FALSE)
     obs.d <- rep(0, length(k))
     names.y <- as.integer(names(y))
@@ -72,19 +87,34 @@
     }
     FDR <- ifelse(obs.d == 0, 0, exp.fd/obs.d)
     fdr.ok <- which(FDR < fdr.cutoff)
+<<<<<<< HEAD
     if (length(fdr.ok) < 1) {
         return(0) ## Return 0 is there are no cutoffs low enough
     }
     fdr.chosen <- fdr.ok[1]
     tmp <- k[fdr.chosen - 1] + (FDR[fdr.chosen - 1] - fdr.cutoff)/(FDR[fdr.chosen - 1] - FDR[fdr.chosen])
     if (length(tmp) > 0) { ## Always give output 0 if tmp is numeric(0)
+=======
+    # Return 0 is there are no cutoffs low enough
+    if (length(fdr.ok) < 1) {
+        return(0)
+    }
+    fdr.chosen <- fdr.ok[1]
+    tmp <- k[fdr.chosen - 1] + (FDR[fdr.chosen - 1] - fdr.cutoff)/(FDR[fdr.chosen - 1] - FDR[fdr.chosen])
+    ## Always give output 0 if tmp is numeric(0)
+    if (length(tmp) > 0) {
+>>>>>>> master
         tmp
     } else {
         0
     }
 }
 
+<<<<<<< HEAD
 .tng <- function(df, use, correctmapa = TRUE, plot = NULL, verbose = TRUE) {
+=======
+.tng <- function(df, use, correctmappa = TRUE, plot = NULL, verbose = TRUE) {
+>>>>>>> master
     #tests
     if (!is.logical(use) && length(use) == nrow(df)) 
         stop("use should be logicval vector with same size as df")
@@ -108,7 +138,11 @@
     #gc fits also excludes the low mappability data
 
     #correct gc using double lowess
+<<<<<<< HEAD
     gcuse <- (use & !is.na(df$mapa) & df$mapa > 0.8 & !is.na(df$gc) & df$gc > 0)
+=======
+    gcuse <- (use & !is.na(df$mappa) & df$mappa > 0.8 & !is.na(df$gc) & df$gc > 0)
+>>>>>>> master
     rough <- loess(count ~ gc, data = df, subset = gcuse, span = 0.03)
     i <- seq(0, 1, by = 0.001)
     final <- loess(predict(rough, i) ~ i, span = 0.3)
@@ -125,6 +159,7 @@
         points(df$gc, normv, col = "red", pch = ".")
     }
 
+<<<<<<< HEAD
     #correct mapa using linear function that intercepts zero
     #if(correctmapa) {
     #mapause <- (use & !is.na(df$mapa))
@@ -160,9 +195,62 @@
         }
 
         return(log2(df$countgcmapaloess/median(df$countgcmapaloess[use],
+=======
+    #correct mappa using linear function that intercepts zero
+    #if(correctmappa) {
+    #mappause <- (use & !is.na(df$mappa))
+    #lm(countgcloess~0+mappa, data=df, subset=mappause) ->fll
+    #if(verbose) print(summary(fll))
+
+    #if (plot) {
+    #  plot(countgcloess ~ mappa, data=df, subset=mappause,
+    #       ylim=quantile(df$countgcloess, c(0.0001, .999), na.rm=T), pch=".")
+    #  points(countgcloess ~ mappa, data=df, subset=!mappause,
+    #         col=rgb(1,0,0,.3), pch=".")
+    #  abline(0, fll$coef, col=2)
+    #}
+
+    #correct mappa using double lowess -> paired end sequencing
+    if (correctmappa) {
+        mappause <- (use & !is.na(df$mappa))
+        rough <- loess(countgcloess ~ mappa, data = df, subset = mappause,
+                       span = 0.03)
+        i <- seq(0, 1, by = 0.001)
+        final <- loess(predict(rough, i) ~ i, span = 0.3)
+        normv <- predict(final, df$mappa)
+        df$countgcmappaloess <- df$countgcloess/(normv/median(normv,
+                                                             na.rm = TRUE))
+
+        if (plot) {
+            plot(countgcloess ~ mappa, data = df, subset = mappause,
+                 ylim = quantile(df$countgcloess[mappause], c(1e-04, 0.999),
+                                 na.rm = TRUE), xlim = c(0, 1), pch = ".")
+            points(countgcloess ~ mappa, data = df, subset = !mappause,
+                   col = rgb(1, 0, 0, 0.3), pch = ".")
+            lines(i, predict(rough, i), col = "green")
+            points(df$mappa, normv,
+                   subset = (!is.na(df$mappa) && !is.na(normv)),
+                   col = "red", pch = ".")
+        }
+
+        return(log2(df$countgcmappaloess/median(df$countgcmappaloess[use],
+>>>>>>> master
                                                na.rm = TRUE)))
     } else {
         #corerct agains median value (exluding sex chr)
         log2(df$countgcloess/median(df$countgcloess[use], na.rm = TRUE))
     }
+<<<<<<< HEAD
 }
+=======
+}
+
+.wrap <- function(...) {
+    file.sep <- .Platform$file.sep
+    splitted <- paste(unlist(strsplit(paste(...), split = file.sep)),
+                                      collapse = paste0(file.sep, " "))
+    splitted.pasted <- paste(strwrap(paste(splitted),
+                                     exdent = 2), collapse = "\n")
+    gsub(paste0(file.sep, " "), file.sep, splitted.pasted)
+}
+>>>>>>> master
